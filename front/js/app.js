57635180306098f9a8a7223966d53428c908201f
Andrzej Stachlewski <andrzej.stachlewski+github@gmail.com>
2015-02-11 23:47:38 +0100

    Implementation ToDo list

--- front/js/app.js
+++ front/js/app.js
@@ -1,6 +1,6 @@
 define([
-    "chat/helpers/Observer",
-    "chat/helpers/Subject",
+    //"chat/helpers/Observer",
+    //"chat/helpers/Subject",
     "chat/helpers/EventBus",
     "chat/helpers/helpers",
 
@@ -12,3 +12,27 @@ define([
     "chat/views/MessageView",
     "chat/main"
 ], function(){});
+
+// Features to do:
+//----------
+// Simple communication
+
+// Remote and local storage
+
+// UserA broadcasts thread proposition
+// UserX, UserY, UserZ... can join thread by using thread code
+
+// Escaping threads
+
+// User must keep track of which threads he participates in
+
+// Filters, eg. `?$$` -> all `$$` messages
+
+// Messages must keep thread reference
+
+// Thread hinting
+
+// Timestamps
+
+// Is writting
+// Sent, received, read
